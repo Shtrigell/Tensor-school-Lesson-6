c3d683750b697ecf7f9e985317b2384bbfc64e74
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-19 21:55:04 +0300

    Update Tensor_school_Lesson_6__2.py

--- Tensor_school_Lesson_6__2.py
+++ Tensor_school_Lesson_6__2.py
@@ -1,4 +1,4 @@
-inp=open('C:\\Users\\Shtrige11\\Desktop\\Input.txt', 'r')
+inp=open('./Input.txt', 'r')
 data=inp.read().splitlines()[1:]
 C=[int(s) for s in data[0].split() if s.isdigit()]
 H=[int(s) for s in data[1].split() if s.isdigit()]
@@ -8,6 +8,6 @@ try:
 except IndexError:
     alcohol=0
 inp.close()
-out=open('C:\\Users\\Shtrige11\\Desktop\\Output.txt','w')
+out=open('./Output.txt','w')
 out.write("Количество молекул спирта равно: "+str(alcohol))
 out.close()
