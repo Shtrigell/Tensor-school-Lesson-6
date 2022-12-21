61a92d73f4fe07a9c50a267603f427367689e75e
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-21 19:35:41 +0300

    Update xor_lab_old.py

--- xor_lab_old.py
+++ xor_lab_old.py
@@ -1,6 +1,14 @@
 import random
 
 def xor_cipher():
+    """Функция для XOR-шифрования.
+    
+    Функция совершает XOR-шифрование и расшифровку.  
+    Принимает строку (зашифрованную, либо ту которую нужно зашифровать).  
+    Возвращает строку (расшифрованную, либо ту которую нужно расшифровать).
+    Вызывает функции: file_reading - для чтения из файла, file_writing - для записи в файл.
+    
+    """
     message = input("Give me your secrets (Enter - read from file): ")
     key = input("Give me your key (Enter - KeyGeneration): ")
 
@@ -25,16 +33,27 @@ def xor_cipher():
     file_writing(result)
 
 def file_writing(data, filename="./text.txt"):
+    """Функция записи файла.  
+   
+    Функция принимает строку - data.
+    Содержит необязательный аргумент, имя файла под запись - filename.
+    Записывает строку data в файл.
+   
+    """
     f = open(filename, "wb")
     f.write(data)
     f.close()
 
 def file_reading(filename="./text.txt"):
+     """Функция чтения файла.  
+   
+    Содержит необязательный аргумент, имя файла для чтения - filename.
+    Возвращает строку data из файла.
+   
+    """
     f = open(filename, "rb")
     data = f.read()
     f.close()
     return data
 
 xor_cipher()
-
-
